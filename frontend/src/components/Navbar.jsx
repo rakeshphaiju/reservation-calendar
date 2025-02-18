ee593934568f73b2792d31b313f60c4201a1162e
Rakesh Phaiju <rakeshphaiju22@gmail.com>
2025-02-18 19:28:11 +0200

    add navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-900 text-white p-4">
+      <ul className="flex space-x-4">
+        <li>
+          <Link to="/" className="hover:text-gray-400">Home</Link>
+        </li>
+        <li>
+          <Link to="/about" className="hover:text-gray-400">About</Link>
+        </li>
+        <li>
+          <Link to="/contact" className="hover:text-gray-400">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
