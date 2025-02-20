70544b9375863704a3b49586333b3d38f51fd716
Rakesh Phaiju <rakeshphaiju22@gmail.com>
2025-02-20 15:47:48 +0200

    Containerization

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+# ---- Frontend Stage ----
+FROM node:20 AS frontend-build
+WORKDIR /app/frontend
+
+# Copy package files and install dependencies using Yarn
+COPY frontend/package.json frontend/yarn.lock ./
+RUN yarn install
+
+# Build the React app
+COPY frontend/ ./
+RUN yarn build
+
+# ---- Backend Stage ----
+FROM python:3.9-slim as backend-build
+# virtualenv
+ENV VIRTUAL_ENV=/opt/venv
+RUN python3 -m venv $VIRTUAL_ENV
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+
+RUN apt-get update
+RUN python3 --version
+RUN python3 -m pip install --no-cache-dir --upgrade pip
+RUN python3 -m pip install --no-cache-dir --upgrade poetry
+RUN python3 -m pip --version
+RUN poetry --version
+
+COPY ./pyproject.toml ./poetry.lock ./
+RUN poetry install --no-root -vvv --no-interaction --no-ansi
+
+
+# Set the working directory in the container
+WORKDIR /opt/app
+
+# Copy the current directory contents into the container at /app
+COPY ./src/ ./src
+
+ENV PYTHONPATH=/opt/app
+
+EXPOSE 8000
+
+CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- frontend/src/components/Reserve.jsx
+++ frontend/src/components/Reserve.jsx
@@ -1,232 +1,378 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./reserve.css";
 import moment from "moment";
 import Modal from "./Modal";
 import Input from "./form/Input";
 import Select from "./form/Select";
 
-const Reserve = () => {
-  const initialState = {
-    user: {
-      name: "",
-      address: "",
-      phone_number: "",
-      ruoka: "",
-      quantity: "",
-    },
-    nameError: "",
-    AddressError: "",
-    phone_numberError: "",
-    ruokaError: "",
-    quantityError: "",
-  };
 
-  const [state, setState] = useState({
-    ...initialState,
-    users: [],
-    FoodOption: ["Option 1", "Option 2"],
-    showModal: false,
-    modalData: { day: "", time: "" },
-    currentDate: new Date(),
-    days: ["Thursday", "Friday", "Saturday", "Sunday"],
-    times: [
-      "17:00-17:30",
-      "17:30-18:00",
-      "18:00-18:30",
-      "18:30-19:00",
-      "19:00-19:30",
-      "19:30-20:00",
-      "20:00-20:30",
-    ],
-    reservedTime: {
-      thursday: [],
-      friday: [],
-      saturday: [],
-      sunday: [],
-    },
-  });
+const initialState = {
+  user: {
+    name: "",
+    address: "",
+    phone_number: "",
+    ruoka: "",
+    quantity: ""
+  },
+  nameError: "",
+  addressError: "",
+  phone_numberError: "",
+  ruokaError: "",
+  quantityError: ""
 
-  const handleInput = (e) => {
-    const { name, value } = e.target;
-    setState((prevState) => ({
-      ...prevState,
+}
+
+
+export default class Reserve extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+
       user: {
-        ...prevState.user,
-        [name]: value,
+        name: "",
+        address: "",
+        phone_number: "",
+        ruoka: "",
+        quantity: ""
       },
-    }));
-  };
+      nameError: "",
+      addressError: "",
+      phone_numberError: "",
+      ruokaError: "",
+      quantityError: "",    
+      FoodOption: ["Option 1", "Option 2 "],
+      showModal: false,
+      modalData: { day: "", time: "" },
 
-  const validate = () => {
-    const { user } = state;
+      currentDate: new Date(),
+
+      days: ["Friday", "Saturday", "Sunday"],
+      times: [
+        "17:00-17:30",
+        "17:30-18:00",
+        "18:00-18:30",
+        "18:30-19:00",
+        "19:00-19:30",
+        "19:30-20:00",
+        "20:00-20:30"
+      ],
+      reservedTime: {
+        friday: [],
+        saturday: [],
+        sunday: []
+      }, 
+    };
+
+    this.handleFullName = this.handleFullName.bind(this);
+    this.handleAddress = this.handleAddress.bind(this);
+    this.handlePhonenumber = this.handlePhonenumber.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.handleConfirmReservation = this.handleConfirmReservation.bind(this);
+  }
+
+  validate = () => {
     let nameError = "";
-    let AddressError = "";
+    let addressError = "";
     let phone_numberError = "";
     let ruokaError = "";
     let quantityError = "";
 
-    if (!user.name) nameError = "Invalid name!!";
-    if (!user.address) AddressError = "Invalid Address!!";
-    if (!user.phone_number.match(/^\d{10}$/)) phone_numberError = "Invalid Phone number!!";
-    if (!user.ruoka) ruokaError = "Invalid food option!!";
-    if (user.quantity < 1) quantityError = "Invalid quantity!!";
+    if(!this.state.user.name){
+      nameError = "Invalid name!!";
+    }
+    if(!this.state.user.address){
+      addressError = "Invalid Address!!";
+    }
+    var phoneno =/^\d{10}$/;
+    if(!this.state.user.phone_number.match(phoneno)){
+      phone_numberError = "Invalid Phone number!!";
+    }
+    if(!this.state.user.ruoka){
+      ruokaError = "Invalid food option!!";
+    }
+    if(this.state.user.quantity < 1 ){
+      quantityError = "Invalid quantity!!";
+    }
 
-    if (nameError || AddressError || phone_numberError || ruokaError || quantityError) {
-      setState((prevState) => ({
-        ...prevState,
-        nameError,
-        AddressError,
-        phone_numberError,
-        ruokaError,
-        quantityError,
-      }));
+    if (nameError || addressError || phone_numberError || ruokaError || quantityError ) {
+      this.setState({ nameError, addressError, phone_numberError, ruokaError, quantityError });
       return false;
     }
+    
     return true;
   };
 
-  const handleConfirmReservation = async (e) => {
+  handleFullName(e) {
+    let value = e.target.value;
+    this.setState(
+      prevState => ({
+        user: {
+          ...prevState.user,
+          name: value
+        }
+      }),
+      () => console.log(this.state.user)
+    );
+  }
+
+  handleAddress(e) {
+    let value = e.target.value;
+    this.setState(
+      prevState => ({
+        user: {
+          ...prevState.user,
+          address: value
+        }
+      }),
+      () => console.log(this.state.user)
+    );
+  }
+
+  handlePhonenumber(e) {
+    let value = e.target.value;
+    this.setState(
+      prevState => ({
+        user: {
+          ...prevState.user,
+          phone_number: value
+        }
+      }),
+      () => console.log(this.state.user)
+    );
+  }
+
+  handleInput(e) {
+    let value = e.target.value;
+    let name = e.target.name;
+    this.setState(
+      prevState => ({
+        user: {
+          ...prevState.user,
+          [name]: value
+        }
+      }),
+      () => console.log(this.state.user)
+    );
+  }
+
+  closeModalHandler = () => {
+    this.setState({
+      showModal: false
+    });
+  };
+
+  // show modal and save the day time in state
+  showForm(day, time) {
+    this.setState({
+      showModal: true,
+      modalData: {
+        day,
+        time
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = _ => {
+    fetch("http://localhost:8000/api/users")
+      .then(res => res.json())
+      .then(json => { 
+        this.setState({ users: json.data })})
+      .catch(err => console.error(err));
+  };
+
+  // reserve time slot after modal confirmation
+  handleConfirmReservation = e => {
     e.preventDefault();
-    if (!validate()) return;
 
-    const { modalData, user } = state;
-    try {
-      await fetch(
-        `/users/add?name=${user.name}&Address=${user.address}&phone_number=${user.phone_number}&ruoka=${user.ruoka}&quantity=${user.quantity}&Day=${modalData.day}&time=${modalData.time}`
-      );
-      getUsers();
-      setState((prevState) => ({
-        ...prevState,
-        showModal: false,
-        modalData: {},
-        ...initialState,
-      }));
-    } catch (err) {
-      console.error(err);
+    const { reservedTime, modalData } = this.state;
+    const loweredCaseDay = modalData.day.toLowerCase();
+    const { user } = this.state;
+    const isValid = this.validate();
+    if (isValid) {
+      fetch("http://localhost:8000/api/users/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: user.name,
+          address: user.address,
+          phone_number: user.phone_number,
+          ruoka: user.ruoka,
+          quantity: user.quantity,
+          day: modalData.day,
+          time: modalData.time,
+        }),
+      })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        this.getUsers();
+      })
+      .catch((err) => console.error("Error:", err));
+
+      if (Object.keys(reservedTime).includes(loweredCaseDay)) {
+        const updatedReservedTime = [
+          ...reservedTime[loweredCaseDay],
+          modalData.time
+        ];
+
+        this.setState(prevState => ({
+          reservedTime: {
+            ...prevState.reservedTime,
+            [loweredCaseDay]: updatedReservedTime
+          },
+          showModal: false,
+          modalData: {},
+        
+        }));
+      }
+      this.setState(initialState);
     }
   };
 
-  const getUsers = async () => {
-    try {
-      const res = await fetch("/users");
-      const json = await res.json();
-      setState((prevState) => ({ ...prevState, users: json.data }));
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  render() {
+    const { currentDate, times, days, reservedTime, modalData, users } = this.state;
 
-  useEffect(() => {
-    getUsers();
-  }, []);
+    const weeknumber = moment(currentDate).week();
+    const fifthOfWeek = moment().startOf('isoWeek').add(4, 'd').format("MM-DD-YYYY");
+    const endOfWeek = moment().endOf('isoWeek').format("MM-DD-YYYY");
 
-  const { currentDate, times, days, reservedTime, modalData, users, showModal } = state;
+    const timeHeader = times.map((time, i) => (
+      <th key={`header-time-${i}`}>{time}</th>
+    ));
 
-  const weeknumber = moment(currentDate).week();
-  const fifthOfWeek = moment().startOf("isoWeek").add(4, "d").format("MM-DD-YYYY");
-  const endOfWeek = moment().endOf("isoWeek").format("MM-DD-YYYY");
-
-  const timeHeader = times.map((time, i) => <th key={`header-time-${i}`}>{time}</th>);
-
-  const timeList = days.map((day) => {
-    const row = times.map((time) => {
-      const reserved = reservedTime[day.toLowerCase()].includes(time);
-      const dbReserved = users.find(
-        (data) =>
-          data.Day.toLowerCase() === day.toLowerCase() && data.time === time
-      );
-
+    const timeList = days.map((day, index) => {
+      let row = [];
+    
+      times.forEach(time => {
+        const reserved = reservedTime[day.toLowerCase()].includes(time);
+        const dbReserved = (users ?? []).find(data => {
+          return data.Day.toLowerCase() === day.toLowerCase() && data.time === time;
+        });
+    
+        row.push(
+          <td key={`${day}-${time}`} bgcolor={reserved || dbReserved ? "red" : "#fff"}>
+            <button
+              disabled={reserved || dbReserved}
+              onClick={() => this.showForm(day, time)}
+            >
+              {reserved || dbReserved ? "Booked" : "Book"}
+            </button>
+          </td>
+        );
+      });
+    
       return (
-        <td key={`${day}-${time}`} bgcolor={reserved || dbReserved ? "red" : "#fff"}>
-          <button
-            disabled={reserved || dbReserved}
-            onClick={() => setState((prevState) => ({ ...prevState, showModal: true, modalData: { day, time } }))}
-          >
-            {reserved || dbReserved ? "Booked" : "Book"}
-          </button>
-        </td>
+        <tr key={index}>
+          <th>{day}</th>
+          {row}
+        </tr>
       );
     });
 
     return (
-      <tr key={day}>
-        <th>{day}</th>
-        {row}
-      </tr>
+      <div>
+       <h3> Week number = {weeknumber}<br/>
+        (From {fifthOfWeek} to {endOfWeek})</h3>
+        <table>
+          <thead>
+            <tr> {/* Wrap the <th> inside a <tr> */}
+              <th></th> {/* Empty <th> for the first column */}
+                {timeHeader}
+            </tr>
+          </thead>
+          <tbody>{timeList}</tbody>
+        </table>
+        <Modal
+          className="modal"
+          show={this.state.showModal}
+          close={this.closeModalHandler}
+        >
+          <p>{`Would you like to reserve ${modalData.time} on ${
+            modalData.day
+          }??`}</p>
+          <form>
+            <Input
+              inputtype={"text"}
+              title={"Full Name"}
+              name={"name"}
+              value={this.state.user.name}
+              placeholder={"Enter your name"}
+              handlechange={this.handleInput}
+              required
+            />{" "}
+            {/* Name of the user */}
+            <div style={{ color: "red", fontSize: 12 }}>
+              {this.state.nameError}
+            </div>
+            <Input
+              inputtype={"text"}
+              title={"Address"}
+              name={"address"}
+              value={this.state.user.address}
+              placeholder={"Enter your address"}
+              handlechange={this.handleInput}
+            />{" "}
+            {/* Address of the user */}
+            <div style={{ color: "red", fontSize: 12 }}>
+              {this.state.addressError}
+            </div>
+            <Input
+              inputtype={"text"}
+              title={"Phone number (0XXXXXXXXX)"}
+              name={"phone_number"}
+              value={this.state.user.phone_number}
+              placeholder={"Enter your phone number"}
+              handlechange={this.handleInput}
+            />{" "}
+            {/* Phone number */}
+            <div style={{ color: "red", fontSize: 12 }}>
+              {this.state.phone_numberError}
+            </div>
+            <Select
+              title={"Option"}
+              name={"ruoka"}
+              options={this.state.FoodOption}
+              value={this.state.user.ruoka}
+              placeholder={"Select Option"}
+              handlechange={this.handleInput}
+            />{" "}
+            <div style={{ color: "red", fontSize: 12 }}>
+              {this.state.ruokaError}
+            </div>
+            <Input
+              inputtype={"text"}
+              title={"Quantity"}
+              name={"quantity"}
+              value={this.state.user.quantity}
+              placeholder={"How about quantity??"}
+              handlechange={this.handleInput}
+              required
+            />{" "}
+            {/* quantity of food */}
+            <div style={{ color: "red", fontSize: 12 }}>
+              {this.state.quantityError}
+            </div>
+            <button
+              className="btn-reserve"
+              onClick={this.handleConfirmReservation}
+            >
+              Reserve
+            </button>
+          </form>
+        </Modal>
+      </div>
     );
-  });
-
-  return (
-    <div>
-      <h3>
-        Week number = {weeknumber}
-        <br />
-        (From {fifthOfWeek} to {endOfWeek})
-      </h3>
-      <table>
-        <thead>
-          <tr>
-            <th />
-            {timeHeader}
-          </tr>
-        </thead>
-        <tbody>{timeList}</tbody>
-      </table>
-      <Modal show={showModal} close={() => setState((prevState) => ({ ...prevState, showModal: false }))}>
-        <p>{`Would you like to reserve ${modalData.time} on ${modalData.day}?`}</p>
-        <form>
-          <Input
-            inputtype="text"
-            title="Full Name"
-            name="name"
-            value={state.user.name}
-            placeholder="Enter your name"
-            handlechange={handleInput}
-          />
-          <div style={{ color: "red", fontSize: 12 }}>{state.nameError}</div>
-          <Input
-            inputtype="text"
-            title="Address"
-            name="Address"
-            value={state.user.address}
-            placeholder="Enter your address"
-            handlechange={handleInput}
-          />
-          <div style={{ color: "red", fontSize: 12 }}>{state.AddressError}</div>
-          <Input
-            inputtype="tel"
-            title="Phone number (0XXXXXXXXX)"
-            name="phone_number"
-            value={state.user.phone_number}
-            placeholder="Enter your phone number"
-            handlechange={handleInput}
-          />
-          <div style={{ color: "red", fontSize: 12 }}>{state.phone_numberError}</div>
-          <Select
-            title="Option"
-            name="ruoka"
-            options={state.FoodOption}
-            value={state.user.ruoka}
-            placeholder="Select Option"
-            handlechange={handleInput}
-          />
-          <div style={{ color: "red", fontSize: 12 }}>{state.ruokaError}</div>
-          <Input
-            inputype="number"
-            title="Quantity"
-            name="quantity"
-            value={state.user.quantity}
-            placeholder="How about quantity??"
-            handlechange={handleInput}
-          />
-          <div style={{ color: "red", fontSize: 12 }}>{state.quantityError}</div>
-          <button className="btn-reserve" onClick={handleConfirmReservation}>
-            Reserve
-          </button>
-        </form>
-      </Modal>
-    </div>
-  );
-};
-
-export default Reserve;
+  }
+}
