d200dde40f52147eea69735cc02880ad8082c741
Rakesh Phaiju <rakeshphaiju22@gmail.com>
2026-02-11 15:18:46 +0200

    Remove unnecessary pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,8 +11,6 @@ export default function App() {
       <div className="p-4">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="/reserve" element={<Reserve />} />
           <Route path="/reservelist" element={<Reservationlist />} />
         </Routes>
@@ -20,8 +18,3 @@ export default function App() {
     </div>
   );
 }
-
-// Dummy Page Components
-const Home = () => <h1 className="text-2xl">🏠 Home Page</h1>;
-const About = () => <h1 className="text-2xl">📖 About Page</h1>;
-const Contact = () => <h1 className="text-2xl">📞 Contact Page</h1>;
